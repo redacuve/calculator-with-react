666deac36ea2db3340b94fbe0df41a1b04a91eb2
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-18 01:13:28 -0500

    fixed bug#1 and bug#2 (error when only has one number and press = button and allow button % to work with only one number)

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,35 +2,42 @@ import Big from 'big.js';
 
 function operate(numberOne, numberTwo, operation) {
   let result;
-  if (Number.isNaN(Number(numberOne)) || Number.isNaN(Number(numberTwo))) {
-    return NaN;
+  if (operation) {
+    if (Number.isNaN(Number(numberOne)) || Number.isNaN(Number(numberTwo))) {
+      return NaN;
+    }
+    const n1 = Big(numberOne || 0);
+    const n2 = Big(numberTwo || 0);
+    switch (operation) {
+      case '+':
+        result = n2.plus(n1);
+        break;
+      case '-':
+        result = n2.minus(n1);
+        break;
+      case '×':
+        result = n2.times(n1);
+        break;
+      case '÷':
+        if (n1.eq(Big(0))) {
+          result = NaN;
+        } else {
+          result = n2.div(n1);
+        }
+        break;
+      case '%':
+        if (numberOne === null) {
+          result = n2.div(100);
+        } else {
+          result = n1.times(n2).div(100);
+        }
+        break;
+      default:
+        break;
+    }
+    return result.toString();
   }
-  const n1 = Big(numberOne || 0);
-  const n2 = Big(numberTwo || 0);
-  switch (operation) {
-    case '+':
-      result = n1.plus(n2);
-      break;
-    case '-':
-      result = n1.minus(n2);
-      break;
-    case '×':
-      result = n1.times(n2);
-      break;
-    case '÷':
-      if (n1.eq(Big(0))) {
-        result = NaN;
-      } else {
-        result = n2.div(n1);
-      }
-      break;
-    case '%':
-      result = n1.times(n2).div(100);
-      break;
-    default:
-      break;
-  }
-  return result.toExponential(18);
+  return numberOne;
 }
 
 export default operate;
